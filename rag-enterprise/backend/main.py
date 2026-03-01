f9c6ac2984d59850ebd8cd6d800a1892eca4b600
agnt17 <adityagupta20042003@gmail.com>
2026-03-01 22:03:15 +0530

    adding public frontend domain

--- rag-enterprise/backend/main.py
+++ rag-enterprise/backend/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://rag-enterprise.vercel.app/"
+    ],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
