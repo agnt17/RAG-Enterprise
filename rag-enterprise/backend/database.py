33807b4be53b250c0b4c4513b2c0152baf9291e3
agnt17 <adityagupta20042003@gmail.com>
2026-03-28 12:46:13 +0530

    inspect bug fix

--- rag-enterprise/backend/database.py
+++ rag-enterprise/backend/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, DateTime, Boolean, Integer, Enum
+from sqlalchemy import create_engine, Column, String, DateTime, Boolean, Integer, Enum, inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
